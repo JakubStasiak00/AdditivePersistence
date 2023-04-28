8434dfeb586b9a989dc4d2e0cf38e5e4362fe15d
JakubStasiak00 <125593509+JakubStasiak00@users.noreply.github.com>
2023-04-28 12:10:28 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <input type="number">
+    <table>
+        <tr>
+            <th>Iteration</th>
+            <th>Sum</th>
+        </tr>
+        <tr>
+            <td>dummy</td>
+            <td>dummy</td>
+        </tr>
+    </table>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+    const userInput = ref(null)
+</script>
+
+<style lang="scss">
+    body {
+        font-family: Helvetica;
+    }
+
+    input {
+        display: block;
+        margin: 0 auto;
+        margin-top: 20vh;
+        margin-bottom: 2rem;
+    }
+
+    table {
+        border-collapse: collapse;
+        font-size: 1.5rem;
+        margin: 0 auto;
+    }
+
+    tr + tr {
+        border-top: 2px solid black;
+    }
+
+</style>
