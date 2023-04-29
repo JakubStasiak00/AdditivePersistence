85a22214b048453469b4d592dbc5766d631a90a5
JakubStasiak00 <125593509+JakubStasiak00@users.noreply.github.com>
2023-04-29 17:13:34 +0200

    changing the premiss of the task - displaying additional persistence and digital root instead of every operation, completing code needed to sum digits and define mentioned variables

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,71 @@
 <template>
-    <input type="number">
-    <table>
-        <tr>
-            <th>Iteration</th>
-            <th>Sum</th>
-        </tr>
-        <tr>
-            <td>dummy</td>
-            <td>dummy</td>
-        </tr>
-    </table>
+    <form action="" @submit.prevent="calculateResult(userInput)">
+        <input type="number" v-model="userInput">
+        <button>Calculate</button>
+    </form>
+
+    <div class="result">
+        <div class="persistence">
+            The additional persistence is: <span> {{ additionalPersistence }} </span>
+        </div>
+        <div class="addRoot">
+            The digital root is: <span> {{ digitalRoot }} </span>
+        </div>
+    </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
-    const userInput = ref(null)
+const userInput = ref(null)
+const additionalPersistence = ref(0)
+const digitalRoot = ref(null)
+
+function splitIntoArray(num) {
+    return Array.from(String(num), Number);
+}
+
+const calculatingSum = arr => {
+    let sum = 0
+
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+
+    return sum
+}
+
+const calculateResult = n => {
+    additionalPersistence.value = 0
+    digitalRoot.value = null
+
+    let digits = splitIntoArray(n)
+    let result = 0
+
+    do {
+        result = calculatingSum(digits)
+        digits = splitIntoArray(result)
+        additionalPersistence.value++
+        console.log(result)
+    } while (result >= 10)
+
+    digitalRoot.value = result
+}
+
+
 </script>
 
 <style lang="scss">
-    body {
-        font-family: Helvetica;
-    }
+body {
+    font-family: Helvetica;
+}
 
-    input {
-        display: block;
-        margin: 0 auto;
-        margin-top: 20vh;
-        margin-bottom: 2rem;
-    }
-
-    table {
-        border-collapse: collapse;
-        font-size: 1.5rem;
-        margin: 0 auto;
-    }
-
-    tr + tr {
-        border-top: 2px solid black;
-    }
-
+form {
+    display: flex;
+    margin: 0 auto;
+    margin-top: 20vh;
+    margin-bottom: 2rem;
+    width: 100%;
+    justify-content: center;
+}
 </style>
