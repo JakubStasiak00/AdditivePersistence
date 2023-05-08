25a3653c4da722172a38f5bdb059a3738315575d
JakubStasiak00 <125593509+JakubStasiak00@users.noreply.github.com>
2023-05-08 14:33:50 +0200

    changed input to text, validating that only numbers can be entered, fixing bug with updating input field value

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
+    <header>
+        <h1>Additive Persistence</h1>
+        <p>Calculate the persistence and digital root just like that !</p>
+    </header>
+
     <form action="" @submit.prevent="calculateResult(userInput)">
-        <input type="number" v-model="userInput">
+        <input type="text" v-model="userInput" @input="inputing">
         <button>Calculate</button>
     </form>
 
@@ -19,7 +24,15 @@ import { ref } from 'vue';
 
 const userInput = ref(null)
 const additionalPersistence = ref(0)
-const digitalRoot = ref(null)
+const digitalRoot = ref(0)
+const numberRegex = /^\d+$/
+
+const inputing = e => {
+    if (!numberRegex.test(e.data)) {
+        e.target.value = e.target.value.replace(/[^0-9]/g, '')
+        userInput.value = e.target.value
+    }
+}
 
 function splitIntoArray(num) {
     return Array.from(String(num), Number);
@@ -37,12 +50,19 @@ const calculatingSum = arr => {
 
 const calculateResult = n => {
     additionalPersistence.value = 0
-    digitalRoot.value = null
+    digitalRoot.value = 0
 
     let digits = splitIntoArray(n)
     let result = 0
 
+    console.log(digits)
+
     do {
+        if (!(digits.length > 1)) {
+            digitalRoot.value = digits[0]
+            additionalPersistence.value = 0
+            return
+        }
         result = calculatingSum(digits)
         digits = splitIntoArray(result)
         additionalPersistence.value++
@@ -56,16 +76,33 @@ const calculateResult = n => {
 </script>
 
 <style lang="scss">
+#app {
+    background-color: rgb(92, 189, 92);
+}
+
 body {
     font-family: Helvetica;
+    text-align: center;
+    margin: 0;
 }
 
 form {
     display: flex;
     margin: 0 auto;
-    margin-top: 20vh;
     margin-bottom: 2rem;
     width: 100%;
     justify-content: center;
+}
+
+@media screen and (min-width: 500px) {
+    #app {
+        max-width: 400px;
+        position: absolute;
+        top: 40%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        padding: 2rem;
+
+    }
 }
 </style>
